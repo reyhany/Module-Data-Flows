38e46de0fce7b1a823ed106bcf745a2d5be376b4
reyhany <breyhanyildiz@gmail.com>
2025-11-28 21:53:34 +0100

    solution2 completed

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -3,16 +3,47 @@
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
-
+const readline = require("readline");
 // 1. Make  a command line interface.
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
 // 2. Make supplies for our speech bubble
 
 // 3. Make a cow that takes a string
+const topLine = "_";
+const bottomLine = "-";
 
 const cow = (saying) => {
-    // how did you make the cow before?
-}
+    if (!saying) saying = "Moo";
+
+    const length = saying.length + 2;
+    const top = topLine.repeat(length);
+    const bottom = bottomLine.repeat(length);
+
+    const bubble =
+        ` ${top}\n` +
+        `< ${saying} >\n` +
+        ` ${bottom}\n`;
+
+    const picture = `
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+    `;
+
+    return bubble + picture;
+};
+
+// 4. Prompt the user
+rl.question("What are you eating? ", function(answer) {
+    console.log(cow(answer));
+    rl.close();
+});
 
 // 4. Use readline to get a string from the terminal 
 // (with a prompt so it's clearer what we want)
