0bb27c7010c7a93e69976caa18713eb2dcd9999e
reyhany <breyhanyildiz@gmail.com>
2025-12-04 09:12:52 +0100

    array destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+ 
+let totalCost = 0;
+
+order.forEach(orderItem => {
+  const { itemName, quantity, unitPricePence } = orderItem;
+  const itemTotal = (quantity * unitPricePence) / 100;
+  totalCost += itemTotal;
+
+  console.log(`${quantity} ${itemName} ${itemTotal.toFixed(2)}`);
+});
+
+console.log(`Total: ${totalCost.toFixed(2)}`);
